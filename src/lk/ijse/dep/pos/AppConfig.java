a96b58ea113b8a8237f7375ddf164da1b06039bf
NadeeshWijesingha <nadeeshwijesingha@gmail.com>
2020-08-27 08:16:16 +0530

    Set package location in @ComponentScan

--- src/lk/ijse/dep/pos/AppConfig.java
+++ src/lk/ijse/dep/pos/AppConfig.java
@@ -3,7 +3,7 @@ package lk.ijse.dep.pos;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@ComponentScan
+@ComponentScan("lk.ijse.dep.pos")
 @Configuration
 public class AppConfig {
 }
